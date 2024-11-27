dd959b7079938ac8afe4e4bfc3e57a33c5e3104a
Dian Prasetyo <dprasmail@gmail.com>
2024-11-27 10:58:47 +0700

    feat: asteroid movement

--- lib/component/asteroid.dart
+++ lib/component/asteroid.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 
 class Asteroid extends SpriteComponent with HasGameRef {
   double asteroidSize = 0.5;
+  late Vector2 direction;
+  late double speed;
 
   Asteroid({required this.asteroidSize}) {
     scale = Vector2(asteroidSize, asteroidSize);
@@ -12,5 +15,27 @@ class Asteroid extends SpriteComponent with HasGameRef {
   FutureOr<void> onLoad() async {
     sprite = await game.loadSprite("meteor/spaceMeteors_001.png");
     anchor = Anchor.center;
+
+    Random random = Random();
+
+    double x = random.nextDouble() * 2 - 1;
+    double y = random.nextDouble() * 2 - 1;
+
+    direction = Vector2(x, y);
+
+    speed = random.nextDouble() * 50;
+  }
+
+  @override
+  void update(double dt) {
+    position.add(direction * speed * dt);
+
+    if (position.x < 0 ||
+        position.x > game.size.x ||
+        position.y < 0 ||
+        position.y > game.size.y) {
+      removeFromParent();
+    }
+    super.update(dt);
   }
 }
