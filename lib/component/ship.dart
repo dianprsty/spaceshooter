295d916185308c26b2fad48f9850ba6d6fcebc34
Dian Prasetyo <dprasmail@gmail.com>
2024-11-27 07:22:14 +0700

    feat: user input tap and pan

--- lib/component/ship.dart
+++ lib/component/ship.dart
@@ -9,22 +9,22 @@ class Ship extends SpriteComponent {
   FutureOr<void> onLoad() async {
     sprite = Sprite(await Flame.images.load("ships/spaceShips_001.png"));
     position = Vector2(100, 200);
-    angle = pi / 1.7;
+    angle = pi * 1.5;
     // size = Vector2(50, 50);
-    scale = Vector2(1.5, 1.5);
+    // scale = Vector2(1.5, 1.5);
     anchor = Anchor.center;
   }
 
   @override
   void update(double dt) {
-    position.add(Vector2(5, 2));
-    if (position.x > 500) {
-      position.x = 0;
-    }
+    // position.add(Vector2(5, 2));
+    // if (position.x > 500) {
+    //   position.x = 0;
+    // }
 
-    if (position.y > 800) {
-      position.y = 0;
-    }
+    // if (position.y > 800) {
+    //   position.y = 0;
+    // }
     super.update(dt);
   }
 }
--- lib/pages/space_shooter_game.dart
+++ lib/pages/space_shooter_game.dart
@@ -1,11 +1,25 @@
 import 'dart:async';
+import 'dart:developer';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:spaceshooter/component/ship.dart';
 
-class SpaceShooterGame extends FlameGame {
+class SpaceShooterGame extends FlameGame with TapCallbacks, PanDetector {
+  late Ship ship;
   @override
   FutureOr<void> onLoad() async {
-    Ship ship = Ship();
+    ship = Ship();
     add(ship);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    log("Tap Down Event ${event.localPosition}");
+    ship.position.add(Vector2(10, 0));
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    ship.position = info.eventPosition.global;
   }
 }
