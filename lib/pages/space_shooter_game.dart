d6ce59599d9c337ab78c062391881f62ea20415f
Dian Prasetyo <dprasmail@gmail.com>
2024-11-27 07:05:42 +0700

    feat: sprite component

--- lib/pages/space_shooter_game.dart
+++ lib/pages/space_shooter_game.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
-import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:spaceshooter/component/ship.dart';
 
 class SpaceShooterGame extends FlameGame {
   @override
   FutureOr<void> onLoad() async {
-    add(SpriteComponent(
-      sprite: await loadSprite('ships/spaceShips_001.png'),
-    ));
+    Ship ship = Ship();
+    add(ship);
   }
 }
